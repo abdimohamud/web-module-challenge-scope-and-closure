8bb9983ced733976a2ea646d8d0b7083c3b3f60c
unknown <abdinajibmohamud@gmail.com>
2020-07-20 12:06:25 -0700

    still working on task 2

--- index.js
+++ index.js
@@ -59,11 +59,11 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+  let points = 3;
+  return Math.floor(Math.random() * Math.floor(points));
 }
+
 
 /* Task 3: finalScore()
 
@@ -79,7 +79,9 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(innFunc, numOfInn){
+    let final = {};
+    let 
 
   /*Code Here*/
 
